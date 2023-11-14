172c6275b4dd69958f96d36020276912c83cd482
sozgultekin9 <sozgultekin@gmail.com>
2023-11-14 19:35:32 +0300

    suat

---
+++ src/test/java/techproed/StepDefinitions/api_step_definition/US01_02/US02GUESTUSER.java
@@ -0,0 +1,63 @@
+package techproed.StepDefinitions.api_step_definition.US01_02;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.response.Response;
+
+import techproed.pojos.us01_us02.ObjectPojo;
+import techproed.pojos.us01_us02.RegisterPojo;
+import techproed.pojos.us01_us02.ResponsePojo;
+import techproed.utilities.ConfigReader;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+import static techproed.base_url.ManagementonSchoolsBaseUrl.setUp;
+import static techproed.base_url.ManagementonSchoolsBaseUrl.spec;
+
+public class US02GUESTUSER {
+
+
+    techproed.pojos.us01_us02.RegisterPojo payload;
+
+    Response response;
+    ResponsePojo actualData;
+    ObjectPojo objectPojo;
+    public static String Register;
+    public static int RegisterPojo;
+
+    @Given("GUEST icin URL duzenlenir")
+    public void guestIcinURLDuzenlenir() {
+
+        //https://managementonschools.com/app/guestUser/registere
+        setUp(ConfigReader.getProperty("UserName"), ConfigReader.getProperty("Password"));
+        spec.pathParams("first", "guestUser", "second", "registere");
+        
+    }
+
+  //  @And("GUEST USER icin payload duzenlenir")
+  //  public void guestUSERIcinPayloadDuzenlenir() {
+        
+   // }
+
+    @When("GUEST USER POST Request gonderilir ve Reponse alinir")
+    public void guestUSERPOSTRequestGonderilirVeReponseAlinir() {
+
+        response = given(spec).body(payload).when().post("{first}/{second}");
+        response.prettyPrint();
+        RegisterPojo = response.jsonPath().getInt("object.RegisterId");
+        actualData = response.as(ResponsePojo.class);
+        
+    }
+
+    @Then("Status Kodun {int} oldugu dogrulanir")
+    public void statusKodunOlduguDogrulanir(int statuscode) {
+        assertEquals(statuscode, response.statusCode());
+
+    }
+
+    @And("GUEST USER icin gelen Response body dogrulanir")
+    public void guestUSERIcinGelenResponseBodyDogrulanir() {
+    }
+}
---
+++ src/test/java/techproed/pojos/us01_us02/ResponsePojo.java
@@ -0,0 +1,55 @@
+package techproed.pojos.us01_us02;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class ResponsePojo implements Serializable {
+	private ObjectPojo object;
+	private String message;
+	private String httpStatus;
+
+	public ResponsePojo() {
+	}
+
+	public ResponsePojo(ObjectPojo object, String message, String httpStatus) {
+		this.object = object;
+		this.message = message;
+		this.httpStatus = httpStatus;
+	}
+
+	public void setObject(ObjectPojo object){
+		this.object = object;
+	}
+
+	public ObjectPojo getObject(){
+		return object;
+	}
+
+	public void setMessage(String message){
+		this.message = message;
+	}
+
+	public String getMessage(){
+		return message;
+	}
+
+	public void setHttpStatus(String httpStatus){
+		this.httpStatus = httpStatus;
+	}
+
+	public String getHttpStatus(){
+		return httpStatus;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"ResponsePojo{" + 
+			"object = '" + object + '\'' + 
+			",message = '" + message + '\'' + 
+			",httpStatus = '" + httpStatus + '\'' + 
+			"}";
+		}
+
+    //public List<Object> getContent() {}
+}
